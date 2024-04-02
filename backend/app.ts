6a0657998f976cbb8842f385f9b5515c785e97c6
yoannBonge <yoannbonge@live.fr>
2024-04-02 10:05:54 +0200

    Modify vercel.json

--- backend/app.ts
+++ backend/app.ts
@@ -27,34 +27,6 @@ connectDB();
 app.use(express.json({ limit: "5mb" }));
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-
-  // Log des en-têtes CORS
-  console.log(
-    "En-tête Access-Control-Allow-Origin:",
-    res.getHeader("Access-Control-Allow-Origin")
-  );
-  console.log(
-    "En-tête Access-Control-Allow-Methods:",
-    res.getHeader("Access-Control-Allow-Methods")
-  );
-  console.log(
-    "En-tête Access-Control-Allow-Headers:",
-    res.getHeader("Access-Control-Allow-Headers")
-  );
-
-  next();
-});
-
 app.use("/api/composers", composersRoutes);
 app.use("/api/auth", userRoutes);
 app.use("/api/mail", mailRoutes);
