8390aced4a1697b3262824acfdaff4e9193b6707
yoannBonge <yoannbonge@live.fr>
2024-04-03 08:34:10 +0200

    Test

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://those-behind-the-music.vercel.app/"],
+    origin: ["https://those-behind-the-music.vercel.app"],
     methods: ["GET", "POST", "PUT"],
     credentials: true,
   })
